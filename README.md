346925aa376e2c73b586e812fd1ee6e66ddace09
Noe-Sanchez <danfoot4567@gmail.com>
2024-06-04 19:22:19 +0000

    Added back, refactored front

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-### How to run
+# How to run
+
+## Frontend
 
 ```bash
+cd frontend
 npm install
+export WEB_PORT=8081
 npx expo-cli start 
+```
+
+## Backend
+
+```bash
+cd backend
+npm install
+npm run dev
 ```
---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 8003;
+const init_time = Date.now() / 1000;
+
+function requestTime(){
+  return Math.floor((Date.now() / 1000)-init_time).toString();
+}
+
+app.use(express.json());
+
+let allowedOrigins = ['http://localhost:8081'];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      let msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+// Routes
+app.get('/', (req, res) => {
+  console.log('Inbound default GET request at ' + requestTime() + ' seconds');
+  res.send('Debug route');
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+})
